ae319b3eaba7bc2b72ec4a001133e9c4b1eac061
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-12-17 19:45:27 +0400

    created users table

---
+++ src/components/Users/Users.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+import './Users.scss';
+import axios from 'axios';
+import {makeStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+
+class Users extends Component {
+  state = {
+    users: []
+  };
+
+  classes = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  fetchUsers = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        this.setState({
+          users: res.data
+        });
+      });
+  };
+
+  // onRowClick = (row) => {
+  //   console.log('cliekd', row);
+  // };
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+
+        <TableContainer component={Paper}>
+          <Table className={this.classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Full name</TableCell>
+                <TableCell align="right">Username</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Address</TableCell>
+                <TableCell align="right">Phone</TableCell>
+                <TableCell align="right">Website</TableCell>
+                <TableCell align="right">Company</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.users.map(row => (
+                <TableRow key={row.name} onClick={this.props.onRowClick(row)}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.username}</TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right">st. {row.address.street}, {row.address.city}</TableCell>
+                  <TableCell align="right">{row.phone}</TableCell>
+                  <TableCell align="right">{row.website}</TableCell>
+                  <TableCell align="right">{row.company.name}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
+}
+
+export default Users;
