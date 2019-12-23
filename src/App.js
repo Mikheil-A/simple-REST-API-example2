f108b67fd0be4b27d62baa26ab2d58845b311ba9
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-12-23 18:52:38 +0400

    worked on opening drawer

--- src/App.js
+++ src/App.js
@@ -1,65 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Users from "./components/Users/Users";
+import User from "./components/User/User";
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 
 
-const App = () => {
 
-  const [state, setState] = React.useState({
-    isDrawerOpened: false,
-    user: {}
-  });
+class App extends Component {
 
-  const toggleDrawer = drawerOpenState => event => {
-    console.log('toggleDrawer');
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDrawerOpened: false,
+      user: {}
+    };
+  }
 
 
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
+  toggleDrawer = (drawerOpenState, userData = {}) => {
+    console.log(2222222, drawerOpenState, userData);
+
+    this.setState({
+      ...this.state,
+      isDrawerOpened: drawerOpenState,
+      user: userData
+    });
+  };
+
+  drawerContent = () => {
+    let content = null;
+    if (this.state.isDrawerOpened) {
+      content = <div>
+        <h1>User info: </h1>
+
+        <p>{this.state.user.name}</p>
+        <p>{this.state.user.username}</p>
+        <p>{this.state.user.email}</p>
+        <p>{this.state.user.phone}</p>
+        <p>{this.state.user.website}</p>
+        <p>{this.state.user.company.name}</p>
+      </div>
     }
 
-    if (state.isDrawerOpened !== drawerOpenState) {
+    return (
+      <div
+        role="presentation"
+        onClick={() => this.toggleDrawer(false)}
+        onKeyDown={() => this.toggleDrawer(false)}
+      >
 
-      setState({...state, isDrawerOpened: drawerOpenState});
-    }
-
+        {content}
+      </div>
+    )
   };
 
-  const testMethod = () => {
-    console.log('tes tmethod');
-  };
 
-  const drawerContent = () => (
-    <div
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
+  render() {
+    return (
+      <div>
+        <Users onRowClick={(clickedUserRecord) => this.toggleDrawer(true, clickedUserRecord)}/>
 
-      <h1>it fucking works</h1>
-
-    </div>
-  );
-
-  const openUserDrawer = (row) => {
-    toggleDrawer(true);
-    testMethod();
-    console.log(2222, row);
-  };
-
-  return (
-    <div>
-      <Users onRowClick={openUserDrawer}/>
-
-      <Button onClick={toggleDrawer(true)}>Open Right</Button>
-
-      <Drawer anchor="right" open={state.isDrawerOpened} onClose={toggleDrawer(false)}>
-        {drawerContent()}
-      </Drawer>
-    </div>
-  );
-};
+        <Drawer anchor="right" open={this.state.isDrawerOpened} onClose={() => this.toggleDrawer(false)}>
+          <User userDataObj={this.state.user}/>
+          {this.drawerContent()}
+        </Drawer>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './User.scss';
 
 
-const User = () => {
+const User = (props) => {
 
   return (
     <div>
       <h1>user works!</h1>
+      <p>{props.userDataObj}</p>
     </div>
   );
 };
